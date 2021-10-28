e50f2a866651782d7b6b4b57f6e912e9b3443b0c
Germano Alves <germanomacedoalves@hotmail.com>
2021-10-28 14:25:32 -0300

    v1

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 module.exports = {
   reactStrictMode: true,
+  trailingSlash: true,
   pageExtensions: ['mdx', 'md', 'jsx', 'js', 'tsx', 'ts'],
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      '/': { page: '/' },
+     }
+  },
+  images: {
+    loader: "default",
+    
+  },
+
 }
